f6fea02aaf6d6c157151cbccce59372bd8f774ac
DanandJohn <DanandJohn>
2016-03-22 16:07:58 -0400

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,47 @@
 # This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+# The data can then be loaded with the rake db:seed ({or created alongside the db with db:setup).
 #
 # Examples:
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Todo.create!({
+title: "Drag me away from you.",
+body: "W/ 100 men or more (It's gonna take a lot)"
+})
+
+Todo.create!({
+title: "Hear the drums echo in the night.",
+body: "The cheetahs only whispers of some quiet conversation"
+})
+
+Todo.create!({
+title: "Stop an old man along the way.",
+body: "Hope to find some old forgotten words, or at least maybe some ancient melodies."
+})
+
+Todo.create!({
+title: "Hurry, boy!",
+body: "It's waiting there for you."
+})
+
+Todo.create!({
+title: "Bless the rains",
+body: "Down in Africa."
+})
+
+Todo.create!({
+title: "Do what's right",
+body: "Sure as Kilimonjaro rises like Olympus above the Sarenghetti."
+})
+
+Todo.create!({
+title: "Hear what's deep inside",
+body: "Frightened of this thing that I've become."
+})
+
+Todo.create!({
+title: "Perform flute solo",
+body: "(flutes)"
+})
